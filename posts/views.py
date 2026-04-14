6ca654495cd9afbe48244d6a87b31b2bda665269
Marinajwb <byun@gmail.com>
2026-04-14 15:56:00 +0900

    comments&likes

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-
+from posts.models import Post
 def feeds(req):
     if not req.user.is_authenticated:
         return redirect('/users/login')
-    return render(req, 'feeds.html')
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(req, 'feeds.html', context)
